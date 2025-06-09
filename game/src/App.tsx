2cf552cfa6953a438291ba7ca312565c7526dea0
Ali salhab <alisalhab258@gmail.com>
2025-06-09 10:09:58 +0300

    building responsive layout

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -1,8 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <div>ali</div>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav " " main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"green"}>
+        nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"red"}>
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"blue"}>
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
